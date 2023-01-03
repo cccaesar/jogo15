f379d2505f3a54da5a714a12bb9ec79b87cc9f62
caio césar rodrigues costa <ccsrrdrgscosta@gmail.com>
2023-01-03 19:38:47 -0300

    alterando embaralhador

--- src/negocio/Embaralhador.java
+++ src/negocio/Embaralhador.java
@@ -2,61 +2,37 @@ package negocio;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Embaralhador {
-	  public static Integer[] oddPermutation(Integer[] items) {
-		    List<Integer[]> permutations = new ArrayList<>();
-		    oddPermutations(items, 0, permutations);
-		    Integer[] permutation = getRandom(permutations);
-		    return permutation;
-	  }
-
-	  private static void oddPermutations(Integer[] items, int index, List<Integer[]> permutations) {
-	    if (index == items.length - 1) {
-	      permutations.add(items.clone());
-	      return;
-	    }
-
-	    for (int i = index; i < items.length; i++) {
-	      if (i % 2 == 1) {
-	        swap(items, index, i);
-	        oddPermutations(items, index + 1, permutations);
-	        swap(items, index, i);
-	      }
-	    }
+	  private static ArrayList<Integer> embaralhar(List<Integer> lista) {
+		  ArrayList<Integer> listaEmbaralhada = new ArrayList<Integer>();
+		  listaEmbaralhada.addAll(lista);
+		  java.util.Collections.shuffle(listaEmbaralhada);
+		  return listaEmbaralhada;
 	  }
 	  
-	  public static Integer[] evenPermutation(Integer[] items) {
-		List<Integer[]> permutations = new ArrayList<>();
-	    evenPermutations(items, 0, permutations);
-	    Integer[] permutation = getRandom(permutations);
-	    return permutation;
-	  }
-
-	  private static void evenPermutations(Integer[] items, int index, List<Integer[]> permutations) {
-	    if (index == items.length - 1) {
-	      permutations.add(items.clone());
-	      return;
-	    }
-
-	    for (int i = index; i < items.length; i++) {
-	      if (i % 2 == 0) {
-	        swap(items, index, i);
-	        evenPermutations(items, index + 1, permutations);
-	        swap(items, index, i);
-	      }
-	    }
-	  }
-
-	  private static void swap(Integer[] items, int i, int j) {
-	    Integer temp = items[i];
-	    items[i] = items[j];
-	    items[j] = temp;
+	  //** retorna true se a permutação for par e false para impar */
+	  public static boolean verificarParidadeDaPermutacao(List<Integer> lista) {
+		  int inversoes=0;
+		    for(int i=0;i<lista.size();i++)
+		        for(int j=i+1;j<lista.size();j++)
+		            if (lista.get(i)>lista.get(j)) inversoes++;
+		  return inversoes%2 == 0;
 	  }
 	  
-	  private static Integer[] getRandom(List<Integer[]> list) {
-		    int rnd = new Random().nextInt(list.size());
-		    return list.get(rnd);
-		}
+	  public static ArrayList<Integer> embaralhamentoPar(List<Integer> lista) {
+		  ArrayList<Integer> listaEmbaralhada = new ArrayList<Integer>();
+		  do {
+			  listaEmbaralhada = embaralhar(lista);
+		  } while (verificarParidadeDaPermutacao(listaEmbaralhada));
+		  return listaEmbaralhada;
+	  }
+	  
+	  public static ArrayList<Integer> embaralhamentoImpar(List<Integer> lista) {
+		  ArrayList<Integer> listaEmbaralhada = new ArrayList<Integer>();
+		  do {
+			  listaEmbaralhada = embaralhar(lista);
+		  } while (verificarParidadeDaPermutacao(listaEmbaralhada) == false);
+		  return listaEmbaralhada;
+	  }
 }
